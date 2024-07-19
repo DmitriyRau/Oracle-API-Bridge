0d53d79b6317183e1a2aec43774fcd0df6bc4f76
Дмитрий Рау <50152231+DmitriyRau@users.noreply.github.com>
2024-07-19 10:13:37 +0500

    Update wsgi.py

--- asd30/wsgi.py
+++ asd30/wsgi.py
@@ -8,17 +8,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-# import oracledb
-
 from django.core.wsgi import get_wsgi_application
-
-# # Путь к каталогу с Oracle Instant Client
-# oracle_instant_client_dir = r"C:\oracle\instantclient_21_14"
-# # Переменные окружения
-# os.environ["ORACLE_HOME"] = oracle_instant_client_dir
-# os.environ["PATH"] = oracle_instant_client_dir + ";" + os.environ["PATH"]
-# # Инициализация Oracle Instant Client
-# oracledb.init_oracle_client(lib_dir=oracle_instant_client_dir)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asd30.settings')
 
