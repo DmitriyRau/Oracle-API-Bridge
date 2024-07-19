668b14648cd7f9fe89ae92231273977d8e075fdd
Дмитрий Рау <50152231+DmitriyRau@users.noreply.github.com>
2024-07-19 10:15:05 +0500

    Update settings.py

--- asd30/settings.py
+++ asd30/settings.py
@@ -49,9 +49,9 @@ INSTALLED_APPS = [
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
-    'rest_framework',
-    'clickhouse_backend',
-    'rest_framework.authtoken',
+	'rest_framework',
+	'clickhouse_backend',
+	'rest_framework.authtoken',
 	'asd_api',
 ]
 
